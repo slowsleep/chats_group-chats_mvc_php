aed7102e8369ce3f28849b8e86adca6c1bef6139
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-23 23:13:11 +0300

    Add table creation

---
+++ code/app/tools/CreateTables.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once __DIR__ . '/../database/DB.php';
+
+function createTableUsers() {
+    try {
+        $db = DB::connect();
+
+        $query = "CREATE TABLE IF NOT EXISTS `users` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `username` varchar(25) NOT NULL,
+            `email` varchar(255) NOT NULL,
+            `password` varchar(255) NOT NULL,
+            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        )";
+
+        $db->exec($query);
+
+        echo "Table users created successfully" . PHP_EOL;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+
+function createTableChats() {
+    try {
+        $db = DB::connect();
+
+        $query = "CREATE TABLE IF NOT EXISTS `chats` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `is_group` boolean NOT NULL,
+            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        )";
+
+        $db->exec($query);
+
+        echo "Table chats created successfully" . PHP_EOL;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+function createTableMessages() {
+    try {
+        $db = DB::connect();
+
+        $query = "CREATE TABLE IF NOT EXISTS `messages` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `content` text NOT NULL,
+            `user_id` int(11) NOT NULL REFERENCES `users` (`id`) ON DELETE CASCADE,
+            `chat_id` int(11) NOT NULL REFERENCES `chats` (`id`) ON DELETE CASCADE,
+            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        )";
+
+        $db->exec($query);
+
+        echo "Table messages created successfully" . PHP_EOL;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+function createTableChatMembers() {
+    try {
+        $db = DB::connect();
+
+        $query = "CREATE TABLE IF NOT EXISTS `chat_members` (
+            `user_id` int(11) NOT NULL REFERENCES `users` (`id`) ON DELETE CASCADE,
+            `chat_id` int(11) NOT NULL REFERENCES `chats` (`id`) ON DELETE CASCADE,
+            `is_admin` boolean NOT NULL
+        )";
+
+        $db->exec($query);
+
+        echo "Table chat_members created successfully" . PHP_EOL;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+function createTableChatMessages() {
+    try {
+        $db = DB::connect();
+
+        $query = "CREATE TABLE IF NOT EXISTS `chat_messages` (
+            `chat_id` int(11) NOT NULL REFERENCES `chats` (`id`) ON DELETE CASCADE,
+            `message_id` int(11) NOT NULL REFERENCES `messages` (`id`) ON DELETE CASCADE
+        )";
+
+        $db->exec($query);
+
+        echo "Table chat_messages created successfully" . PHP_EOL;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+
+createTableUsers();
+createTableChats();
+createTableMessages();
+createTableChatMembers();
+createTableChatMessages();
