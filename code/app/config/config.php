9c0609c679dec6197cfa3afcb827f34019c661b8
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-24 19:38:22 +0300

    Add APP_DIR to config

--- code/app/config/config.php
+++ code/app/config/config.php
@@ -5,3 +5,4 @@ $env = parse_ini_file(realpath(__DIR__ . '/../../.env'));
 define('DB_NAME', $env['DB_NAME']);
 define('DB_USER', $env['DB_USER']);
 define('DB_PASS', $env['DB_PASSWORD']);
+define('APP_DIR', realpath(__DIR__ . '/..'));
