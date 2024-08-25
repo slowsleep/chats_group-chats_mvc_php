a51c2ed1d5a82bb06ace01e7e1b9471ddab22fa2
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-25 11:47:12 +0300

    Add styles

--- code/app/views/layout/layout_view.php
+++ code/app/views/layout/layout_view.php
@@ -4,15 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app/assets/css/style.css">
     <title><?php echo $title; ?></title>
 </head>
 
 <body>
-    <p>header</p>
-    <hr>
-    <?php include_once APP_DIR . '/views/' . $content_view; ?>
-    <hr>
-    <p>footer</p>
+    <main>
+        <div class="chat-list">
+            <div class="chat-list__dialogs">
+                <p>1</p>
+            </div>
+            <hr>
+            <div class="chat-list__groups">
+                <p>2</p>
+            </div>
+        </div>
+        <hr>
+        <div class="content">
+            <?php include_once APP_DIR . '/views/' . $content_view; ?>
+        </div>
+        <hr>
+        <div class="side-menu">
+            <ul class="side-menu__list">
+                <li class="side-menu__list__item">Профиль</li>
+                <li class="side-menu__list__item">Настройки</li>
+                <li class="side-menu__list__item">Создать группу</li>
+                <li class="side-menu__list__item">Выйти</li>
+            </ul>
+        </div>
+    </main>
 </body>
 
 </html>
