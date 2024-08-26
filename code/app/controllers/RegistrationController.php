a3eb0cf777895d8648b714c4dc101ab876bf1d81
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-26 11:22:08 +0300

    Add register method to RegistrationController.

--- code/app/controllers/RegistrationController.php
+++ code/app/controllers/RegistrationController.php
@@ -3,11 +3,23 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Models\User;
 
 class RegistrationController extends Controller
 {
     public function index()
     {
-        $this->view->render('registration_view.php');
+        $this->view->render(['content_view' => 'registration_view.php', 'title' => 'Регистрация']);
+    }
+
+    public function register()
+    {
+        $user = User::create($_POST);
+
+        if ($user) {
+            $this->view->render(['content_view' => 'registration_view.php', 'data' => 'Регистрация прошла успешно!']);
+        } else {
+            $this->view->render(['content_view' => 'registration_view.php', 'data' => 'Регистрация не прошла!']);
+        }
     }
 }
