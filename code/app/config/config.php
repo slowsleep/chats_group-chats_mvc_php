5ab40f0c528cf474066744505533b4ccc567c512
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-26 11:06:56 +0300

    Add consts DB_HOST and SALT to config

--- code/app/config/config.php
+++ code/app/config/config.php
@@ -2,7 +2,9 @@
 
 $env = parse_ini_file(realpath(__DIR__ . '/../../.env'));
 
+define('DB_HOST', $env['DB_HOST']);
 define('DB_NAME', $env['DB_NAME']);
 define('DB_USER', $env['DB_USER']);
 define('DB_PASS', $env['DB_PASSWORD']);
 define('APP_DIR', realpath(__DIR__ . '/..'));
+define('SALT', $env['APP_SECRET_KEY']);
