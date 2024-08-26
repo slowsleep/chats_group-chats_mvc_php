575e4135a7f156cec308aae63d5380c604ee37be
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-26 11:18:51 +0300

    Add the User model and the create method

---
+++ code/app/models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+use App\Database\DB;
+
+use PDOException;
+
+class User extends Model
+{
+
+    public static function create($data)
+    {
+        try {
+            $password = $data['password'];
+
+            $db = DB::connect();
+
+            $query = 'INSERT INTO users (username, email, password) VALUES (:username, :email, :password)';
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':username', $data['username']);
+            $stmt->bindParam(':email', $data['email']);
+            $hash_password = password_hash($password . SALT, PASSWORD_DEFAULT);
+            $stmt->bindParam(':password', $hash_password);
+            $stmt->execute();
+
+            if ($stmt) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return false;
+    }
+}
