e78c725e8e7f84a226920199947d51f7c19fbf21
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-26 12:47:54 +0300

    Edit layout view: split layout into component parts

--- code/app/views/layout/layout_view.php
+++ code/app/views/layout/layout_view.php
@@ -10,41 +10,11 @@
 
 <body>
     <main>
-        <?php if (isset($_SESSION['user']) && $_SESSION['user']) : ?>
-            <div class="chat-list">
-                <div class="chat-list__dialogs">
-                    <p>1</p>
-                </div>
-                <hr>
-                <div class="chat-list__groups">
-                    <p>2</p>
-                </div>
-            </div>
-            <hr>
-        <?php endif; ?>
+        <?php include_once APP_DIR . '/views/layout/chat-list.php' ?>
         <div class="content">
             <?php include_once APP_DIR . '/views/' . $content_view; ?>
         </div>
-        <hr>
-        <div class="side-menu">
-            <?php if (isset($_SESSION['user']) && $_SESSION['user']) : ?>
-            <ul class="side-menu__list">
-                <li class="side-menu__list__item">Профиль</li>
-                <li class="side-menu__list__item">Настройки</li>
-                <li class="side-menu__list__item">Создать группу</li>
-                <li class="side-menu__list__item"><a href="/logout">Выйти</a></li>
-            </ul>
-            <?php else : ?>
-            <ul class="side-menu__list">
-                <li class="side-menu__list__item">
-                    <a href="/login">Войти</a>
-                </li>
-                <li class="side-menu__list__item">
-                    <a href="/registration">Зарегестрироваться</a>
-                </li>
-            </ul>
-            <?php endif; ?>
-        </div>
+        <?php include_once APP_DIR . '/views/layout/side-menu.php' ?>
     </main>
 </body>
 
---
+++ code/app/views/layout/side-menu.php
@@ -0,0 +1,20 @@
+<hr>
+<div class="side-menu">
+    <?php if (isset($_SESSION['user']) && $_SESSION['user']) : ?>
+        <ul class="side-menu__list">
+            <li class="side-menu__list__item">Профиль</li>
+            <li class="side-menu__list__item">Настройки</li>
+            <li class="side-menu__list__item">Создать группу</li>
+            <li class="side-menu__list__item"><a href="/logout">Выйти</a></li>
+        </ul>
+    <?php else : ?>
+        <ul class="side-menu__list">
+            <li class="side-menu__list__item">
+                <a href="/login">Войти</a>
+            </li>
+            <li class="side-menu__list__item">
+                <a href="/registration">Зарегестрироваться</a>
+            </li>
+        </ul>
+    <?php endif; ?>
+</div>
