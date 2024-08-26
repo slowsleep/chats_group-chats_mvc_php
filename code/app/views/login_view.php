0886d6c84670a7f768bf9923adc6a80995801217
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-26 12:37:35 +0300

    Edit login view

--- code/app/views/login_view.php
+++ code/app/views/login_view.php
@@ -1,15 +1,16 @@
-<form class="form-login" method="post">
+<h1>Вход</h1>
+<form class="form-login" method="post" action="/login/login">
     <div class="form-login__row">
         <label for="login-email">
             Email:
         </label>
-        <input type="email" name="email" id="login-email">
+        <input type="email" name="email" id="login-email" required>
     </div>
     <div class="form-login__row">
         <label for="login-password">
             Пароль:
         </label>
-        <input type="password" name="password" id="login-password">
+        <input type="password" name="password" id="login-password" required>
     </div>
     <input type="submit" value="Войти">
 </form>
