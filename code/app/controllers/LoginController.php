baa3081816d2c3b7a128717a05797896a9fae455
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-26 12:37:05 +0300

    Add login method to LoginController

--- code/app/controllers/LoginController.php
+++ code/app/controllers/LoginController.php
@@ -3,11 +3,23 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Models\User;
 
 class LoginController extends Controller
 {
     public function index()
     {
         $this->view->render(['content_view' => 'login_view.php']);
+    }
+
+    public function login()
+    {
+        $user = User::login($_POST['email'], $_POST['password']);
+        if ($user) {
+            $_SESSION['user'] = $user;
+            header('Location: /home');
+        } else {
+            $this->view->render(['content_view' => 'login_view.php', 'data' => ['message' => 'Неверные данные']]);
+        }
     }
 }
