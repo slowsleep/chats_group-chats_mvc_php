72bab73a67d0c968cb328b7e7bf820ab5b4848d2
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-27 17:11:40 +0300

    Add rule of not using repeated email when registering

--- code/app/controllers/RegistrationController.php
+++ code/app/controllers/RegistrationController.php
@@ -25,8 +25,11 @@ class RegistrationController extends Controller
         }
 
         $errors = [];
+
         if (User::checkUsernameExists($_POST['username'])) {
             $errors['username'] = 'Пользователь с таким именем уже существует!';
+        } else if (User::checkEmailExists($_POST['email'])) {
+            $errors['email'] = 'Пользователь с таким email уже существует!';
         } else {
             $user = User::create($_POST);
         }
