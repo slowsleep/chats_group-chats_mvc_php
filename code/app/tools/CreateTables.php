a34c2e470dece809d5589bba608d3abd2c213767
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-27 17:16:36 +0300

    Edit CreateTables file. Remove quotes. Add avatar and email_confirmed columns to the user table

--- code/app/tools/CreateTables.php
+++ code/app/tools/CreateTables.php
@@ -10,14 +10,16 @@ function createTableUsers()
     try {
         $db = DB::connect();
 
-        $query = "CREATE TABLE IF NOT EXISTS `users` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `username` varchar(25) NOT NULL,
-            `email` varchar(255) NOT NULL,
-            `password` varchar(255) NOT NULL,
-            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            PRIMARY KEY (`id`)
+        $query = "CREATE TABLE IF NOT EXISTS users (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            username varchar(25) NOT NULL,
+            email varchar(255) NOT NULL,
+            password varchar(255) NOT NULL,
+            avatar varchar(255),
+            email_confirmed boolean NOT NULL DEFAULT FALSE,
+            created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (id)
         )";
 
         $db->exec($query);
@@ -34,12 +36,12 @@ function createTableChats()
     try {
         $db = DB::connect();
 
-        $query = "CREATE TABLE IF NOT EXISTS `chats` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `is_group` boolean NOT NULL,
-            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            PRIMARY KEY (`id`)
+        $query = "CREATE TABLE IF NOT EXISTS chats (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            is_group boolean NOT NULL,
+            created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (id)
         )";
 
         $db->exec($query);
@@ -55,14 +57,14 @@ function createTableMessages()
     try {
         $db = DB::connect();
 
-        $query = "CREATE TABLE IF NOT EXISTS `messages` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `content` text NOT NULL,
-            `user_id` int(11) NOT NULL REFERENCES `users` (`id`) ON DELETE CASCADE,
-            `chat_id` int(11) NOT NULL REFERENCES `chats` (`id`) ON DELETE CASCADE,
-            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-            PRIMARY KEY (`id`)
+        $query = "CREATE TABLE IF NOT EXISTS messages (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            content text NOT NULL,
+            user_id int(11) NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+            chat_id int(11) NOT NULL REFERENCES chats (id) ON DELETE CASCADE,
+            created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (id)
         )";
 
         $db->exec($query);
@@ -78,10 +80,10 @@ function createTableChatMembers()
     try {
         $db = DB::connect();
 
-        $query = "CREATE TABLE IF NOT EXISTS `chat_members` (
-            `user_id` int(11) NOT NULL REFERENCES `users` (`id`) ON DELETE CASCADE,
-            `chat_id` int(11) NOT NULL REFERENCES `chats` (`id`) ON DELETE CASCADE,
-            `is_admin` boolean NOT NULL
+        $query = "CREATE TABLE IF NOT EXISTS chat_members (
+            user_id int(11) NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+            chat_id int(11) NOT NULL REFERENCES chats (id) ON DELETE CASCADE,
+            is_admin boolean NOT NULL
         )";
 
         $db->exec($query);
@@ -97,9 +99,9 @@ function createTableChatMessages()
     try {
         $db = DB::connect();
 
-        $query = "CREATE TABLE IF NOT EXISTS `chat_messages` (
-            `chat_id` int(11) NOT NULL REFERENCES `chats` (`id`) ON DELETE CASCADE,
-            `message_id` int(11) NOT NULL REFERENCES `messages` (`id`) ON DELETE CASCADE
+        $query = "CREATE TABLE IF NOT EXISTS chat_messages (
+            chat_id int(11) NOT NULL REFERENCES chats (id) ON DELETE CASCADE,
+            message_id int(11) NOT NULL REFERENCES messages (id) ON DELETE CASCADE
         )";
 
         $db->exec($query);
