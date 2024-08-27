94451f95f823494aa12aa3e7799dc8cce773aab1
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-27 16:58:58 +0300

    Edit class names for form and its elements

--- code/app/views/login_view.php
+++ code/app/views/login_view.php
@@ -2,15 +2,15 @@
 use function App\Tools\generateCsrfToken;
 ?>
 <h1>Вход</h1>
-<form class="form-login" method="post" action="/login/login">
+<form class="form" method="post" action="/login/login">
     <input type="hidden" name="csrf_token" value="<?= generateCsrfToken() ?>">
-    <div class="form-login__row">
+    <div class="form__row">
         <label for="login-email">
             Email:
         </label>
         <input type="email" name="email" id="login-email" required>
     </div>
-    <div class="form-login__row">
+    <div class="form__row">
         <label for="login-password">
             Пароль:
         </label>
--- code/app/views/registration_view.php
+++ code/app/views/registration_view.php
@@ -2,27 +2,27 @@
 use function App\Tools\generateCsrfToken;
 ?>
 <h1>Регистрация</h1>
-<form class="form-registration" method="post" action="/registration/register">
+<form class="form" method="post" action="/registration/register">
     <input type="hidden" name="csrf_token" value="<?= generateCsrfToken() ?>">
-    <div class="form-registration__row">
+    <div class="form__row">
         <label for="reg-username">
             Username:
         </label>
         <input type="text" name="username" id="reg-username" required>
     </div>
-    <div class="form-registration__row">
+    <div class="form__row">
         <label for="reg-email">
             Email:
         </label>
         <input type="email" name="email" id="reg-email" required>
     </div>
-    <div class="form-registration__row">
+    <div class="form__row">
         <label for="reg-password">
             Пароль:
         </label>
         <input type="password" name="password" id="reg-password" required>
     </div>
-    <div class="form-registration__row">
+    <div class="form__row">
         <label for="reg-password-repeat">
             Повтор пароля:
         </label>
