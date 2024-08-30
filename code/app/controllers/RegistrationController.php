607278cce3433fab1e5f686d429098b9cb2aa202
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-30 12:40:34 +0300

    Edit register method of RegistrationController: send activation email to user on registration. Add activate action method

--- code/app/controllers/RegistrationController.php
+++ code/app/controllers/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Core\Controller;
 use App\Models\User;
+use App\Tools\Mail;
 use function App\Tools\validateCsrfToken;
 use function App\Tools\refreshCsrfToken;
 
@@ -31,15 +32,28 @@ class RegistrationController extends Controller
         } else if (User::checkEmailExists($_POST['email'])) {
             $errors['email'] = 'Пользователь с таким email уже существует!';
         } else {
-            $user = User::create($_POST);
+            $email_confirm_token = User::create($_POST);
+            $mail = new Mail();
+            $mail->send($_POST['email'], $email_confirm_token);
         }
 
-        if (isset($user) && $user) {
+        if (isset($email_confirm_token) && $email_confirm_token) {
             $this->view->render(['content_view' => 'registration_view.php', 'data' => ['message' => 'Регистрация прошла успешно!']]);
         } else {
             $this->view->render(['content_view' => 'registration_view.php', 'data' => ['message' => 'Регистрация не прошла!', 'errors' => $errors]]);
         }
 
         refreshCsrfToken();
+    }
+
+    public function activate()
+    {
+        $token = $_GET['token'] ?? '';
+        $user = User::activate($token);
+        if ($user) {
+            $this->view->render(['content_view' => 'registration_view.php', 'data' => ['message' => 'Аккаунт успешно активирован!']]);
+        } else {
+            $this->view->render(['content_view' => 'registration_view.php', 'data' => ['message' => 'Активация аккаунта не удалась!']]);
+        }
     }
 }
