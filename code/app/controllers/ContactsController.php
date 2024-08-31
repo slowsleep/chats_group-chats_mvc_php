28e315e8492e2419ae29838af10ab7e74c0e09fd
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-08-31 12:32:55 +0300

    Add check auth to contacts page

--- code/app/controllers/ContactsController.php
+++ code/app/controllers/ContactsController.php
@@ -9,11 +9,15 @@ class ContactsController extends Controller
 {
     public function index()
     {
+        parent::auth();
+
         $this->view->render(['content_view' => 'contacts_view.php', 'title' => 'Контакты']);
     }
 
     public function search()
     {
+        parent::auth();
+        
         $users = User::getUsers($_GET['search']);
         $this->view->render(['content_view' => 'contacts_view.php', 'title' => 'Контакты', 'data' => ['users' => $users]]);
     }
