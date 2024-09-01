bf6cab09c88ca8bb3338fa7ac0f7619c9769ba76
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-01 20:43:26 +0300

    Edit profile view: add: display email; subscribe and unsubscribe buttons

--- code/app/views/profile_view.php
+++ code/app/views/profile_view.php
@@ -3,7 +3,26 @@
     <?php if (!empty($data['user'])): ?>
     <img src="<?= $data['user']['avatar'] ? '/app/uploads/' . $data['user']['avatar'] : '/app/assets/img/avatar.png' ?>" alt="Avatar">
     <p>Username: <?= $data['user']['username'] ?></p>
+    <p>Email: <?= $data['user']['email'] ?></p>
     <?php else: ?>
         <p>Такой пользователь не найден.</p>
     <?php endif; ?>
 </div>
+
+<?php if (isset($data['user']) && $data['user'] && $data['user']['id'] != $_SESSION['user']['id']): ?>
+<div>
+    <?php if ($data['isSubscribed']): ?>
+        <form action="/profile/unsubscribe" method="post">
+            <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?? '' ?>">
+            <input type="hidden" name="user" value="<?= $data['user']['id'] ?>">
+            <button>Отписаться</button>
+        </form>
+    <?php else: ?>
+        <form action="/profile/subscribe" method="post">
+            <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?? '' ?>">
+            <input type="hidden" name="user" value="<?= $data['user']['id'] ?>">
+            <button>Подписаться</button>
+        </form>
+    <?php endif; ?>
+</div>
+<?php endif; ?>
