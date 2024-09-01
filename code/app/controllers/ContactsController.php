e402bd5d7ab95527a54039f610be44bad84dedd1
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-01 20:38:48 +0300

    Edit ContactsController: display users subscribed by an authorized user

--- code/app/controllers/ContactsController.php
+++ code/app/controllers/ContactsController.php
@@ -4,20 +4,20 @@ namespace App\Controllers;
 
 use App\Core\Controller;
 use App\Models\User;
+use App\Models\Subscription;
 
 class ContactsController extends Controller
 {
     public function index()
     {
         parent::auth();
-
-        $this->view->render(['content_view' => 'contacts_view.php', 'title' => 'Контакты']);
+        $subscribers = Subscription::getSubscriptions($_SESSION['user']['id']);
+        $this->view->render(['content_view' => 'contacts_view.php', 'title' => 'Контакты', 'data' => ['users' => $subscribers]]);
     }
 
     public function search()
     {
         parent::auth();
-        
         $users = User::getUsers($_GET['search']);
         $this->view->render(['content_view' => 'contacts_view.php', 'title' => 'Контакты', 'data' => ['users' => $users]]);
     }
