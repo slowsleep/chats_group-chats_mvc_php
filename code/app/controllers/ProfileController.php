6fcbcde27f5549f27e50ba50f3eb551fb9b3db45
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-01 20:41:39 +0300

    Edit ProfileController, edit index method: add check for user subscription. Add subsribe and unsubscribe methods

--- code/app/controllers/ProfileController.php
+++ code/app/controllers/ProfileController.php
@@ -4,18 +4,67 @@ namespace App\Controllers;
 
 use App\Core\Controller;
 use App\Models\User;
+use App\Models\Subscription;
+use function App\Tools\validateCsrfToken;
+use function App\Tools\refreshCsrfToken;
 
 class ProfileController extends Controller
 {
     public function index()
     {
         parent::auth();
-        
+        refreshCsrfToken();
+
         if (isset($_GET['user'])) {
             $user = User::getUser($_GET['user']);
-            $this->view->render(['content_view' => 'profile_view.php', 'title' => 'Профиль', 'data' => ['user' => $user]]);
+            $isSubscribed = $user && Subscription::read([
+                'user_id' => $_SESSION['user']['id'],
+                'subscribed_to_user_id' => $user['id']
+            ]);
+            $this->view->render(['content_view' => 'profile_view.php', 'title' => 'Профиль', 'data' => ['user' => $user, 'isSubscribed' => $isSubscribed]]);
         } else {
             $this->view->render(['content_view' => 'profile_view.php', 'title' => 'Профиль']);
         }
+    }
+
+    public function subscribe()
+    {
+        parent::auth();
+        $csrfToken = $_POST['csrf_token'] ?? '';
+
+        if (!validateCsrfToken($csrfToken)) {
+            refreshCsrfToken();
+            $this->view->render(['content_view' => 'profile_view.php', 'data' => ['message' => 'Проверка токена CSRF не удалась.']]);
+            exit;
+        }
+
+        $data = [
+            'user_id' => $_SESSION['user']['id'],
+            'subscribed_to_user_id' => $_POST['user']
+        ];
+
+        Subscription::create($data);
+        header("Location: /profile?user=" . $_POST['user']);
+    }
+
+    public function unsubscribe()
+    {
+        parent::auth();
+
+        $csrfToken = $_POST['csrf_token'] ?? '';
+
+        if (!validateCsrfToken($csrfToken)) {
+            refreshCsrfToken();
+            $this->view->render(['content_view' => 'profile_view.php', 'data' => ['message' => 'Проверка токена CSRF не удалась.']]);
+            exit;
+        }
+
+        $data = [
+            'user_id' => $_SESSION['user']['id'],
+            'subscribed_to_user_id' => $_POST['user']
+        ];
+
+        Subscription::destroy($data);
+        header("Location: /profile?user=" . $_POST['user']);
     }
 }
