845857b32c3f22241eafd476f2eff93afc3c1f98
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-03 23:31:34 +0300

    Add display of chat messages. Add send action to ChatController. Edit chat view, add chat styles

--- code/app/controllers/ChatController.php
+++ code/app/controllers/ChatController.php
@@ -5,6 +5,9 @@ namespace App\Controllers;
 use App\Core\Controller;
 use App\Models\User;
 use App\Models\Chat;
+use App\Models\Message;
+use function App\Tools\validateCsrfToken;
+use function App\Tools\refreshCsrfToken;
 
 class ChatController extends Controller
 {
@@ -30,10 +33,51 @@ class ChatController extends Controller
             if (!$chatId) {
                 $chatId = Chat::create(['user_id' => $_SESSION['user']['id'], 'contact_id' => $contactId, 'is_group' => 0]);
             }
+
+            $messages = Chat::getMessages(['chat_id' => $chatId]);
         } else {
             $errors['user'] = 'Пользователь не найден';
         }
 
-        $this->view->render(['content_view' => 'chat_view.php', 'title' => 'Чат', 'data' => ['chat_id' => $chatId, 'contact_name' => $contactName, 'messages' => $messages, 'errors' => $errors]]);
+        $this->view->render([
+            'content_view' => 'chat_view.php',
+            'title' => 'Чат',
+            'data' => [
+                'chat_id' => $chatId,
+                'contact' => [
+                    'contact_id' => $contactId,
+                    'contact_name' => $contactName
+                ],
+                'messages' => $messages,
+                'errors' => $errors
+                ]
+        ]);
+    }
+
+    public function send()
+    {
+        parent::auth();
+
+        $csrfToken = $_POST['csrf_token'] ?? '';
+
+        if (!validateCsrfToken($csrfToken)) {
+            refreshCsrfToken();
+            $this->view->render(['content_view' => 'chat_view.php', 'data' => ['errors' => ['message' => 'Проверка токена CSRF не удалась.']]]);
+            exit;
+        }
+
+        $chatId = $_POST['chat_id'];
+        $content = $_POST['content'];
+        $userId = $_SESSION['user']['id'];
+
+        $newMsg = Message::create(['chat_id' => $chatId, 'user_id' => $userId, 'content' => $content]);
+
+        refreshCsrfToken();
+
+        if ($newMsg) {
+            header('Location: /chat?user=' . $_POST['contact_id']);
+        } else {
+            $this->view->render(['content_view' => 'chat_view.php', 'data' => ['message' => 'Не удалось отправить сообщение']]);
+        }
     }
 }
--- code/app/views/chat_view.php
+++ code/app/views/chat_view.php
@@ -1,14 +1,42 @@
-<div>
-    <h1>Диалог с <?= $data['contact_name'] ?? 'никем' ?></h1>
+<?php
+function isOwnMessage($message)
+{
+    return $message['user_id'] == $_SESSION['user']['id'];
+}
+?>
 
-    <?php if ($data['errors']): ?>
-        <?php foreach ($data['errors'] as $error): ?>
-            <p><?= $error ?></p>
-        <?php endforeach; ?>
+<div class="chat">
+    <h1>Диалог с <?= $data['contact']['contact_name'] ?? 'никем' ?></h1>
+
+    <?php if (isset($data['errors']) && $data['errors']): ?>
+        <div>
+            <?php foreach ($data['errors'] as $error): ?>
+                <p><?= $error ?></p>
+            <?php endforeach; ?>
+        </div>
     <?php endif; ?>
 
-    <form action="">
-        <input type="text" placeholder="Ваше сообщение...">
+    <div class="messages">
+        <?php if (isset($data['messages']) && $data['messages']): ?>
+            <?php foreach ($data['messages'] as $message): ?>
+                <div class="message <?= isOwnMessage($message) ? 'message--own' : '' ?>">
+
+                <div class="message__header">
+                    <p><?= isOwnMessage($message) ? $_SESSION['user']['username'] : $data['contact']['contact_name'] ?></p>
+                    <p><?= $message['created_at'] ?></p>
+                </div>
+
+                    <p><?= $message['content'] ?></p>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+
+    <form class="form" action="chat/send" method="post">
+        <input type="hidden" name="chat_id" value="<?= $data['chat_id'] ?? '' ?>">
+        <input type="hidden" name="contact_id" value="<?= $data['contact']['contact_id'] ?? '' ?>">
+        <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?? '' ?>">
+        <input type="text" name="content" placeholder="Ваше сообщение...">
         <button>Отправить</button>
     </form>
 </div>
