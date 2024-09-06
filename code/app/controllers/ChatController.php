0b3e7b8eff426e35879bb90435a3a2c8376c1185
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-06 18:31:35 +0300

    Edit index method of ChatController: add user's chat list

--- code/app/controllers/ChatController.php
+++ code/app/controllers/ChatController.php
@@ -14,8 +14,41 @@ class ChatController extends Controller
     public function index()
     {
         parent::auth();
+        $usersChats = User::getChats(false);
+        $chats = [];
+        $chatsWithNames = [];
+
+        if ($usersChats) {
+            foreach ($usersChats as $chatId) {
+                $chatMembers = Chat::getMembers($chatId['chat_id']);
+                $chats[] = ['chat_id' => $chatId['chat_id'], 'members' => $chatMembers];
+            }
+
+            foreach ($chats as $chat) {
+                foreach ($chat['members'] as $member) {
+                    $memberInfo = User::getUser($member['user_id']);
+                    if ($memberInfo['id'] != $_SESSION['user']['id']) {
+                        $chatsWithNames[] = [
+                            'id' => $chat['chat_id'],
+                            'user_id' => $memberInfo['id'],
+                            'title' => $memberInfo['username'] != '' ? $memberInfo['username'] : $memberInfo['email']
+                        ];
+                    }
+                }
+            }
+        }
+
+        if (!isset($_GET['user'])) {
+            $this->view->render([
+                'content_view' => '/chat/chat_view.php',
+                'title' => 'Чат',
+                'data' => ['chats' => $chatsWithNames]
+            ]);
+            exit;
+        }
 
         $contactId = $_GET['user'];
+
         $contact = User::getUser($contactId);
         $contactName = null;
 
@@ -40,10 +73,11 @@ class ChatController extends Controller
         }
 
         $this->view->render([
-            'content_view' => 'chat_view.php',
+            'content_view' => '/chat/chat_view.php',
             'title' => 'Чат',
             'data' => [
                 'chat_id' => $chatId,
+                'chats' => $chatsWithNames,
                 'contact' => [
                     'id' => $contactId,
                     'name' => $contactName
