40bbe01372a00bc922f15c06a4197b1e47f359f9
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-08 01:38:55 +0300

    Edit popup view

--- code/app/views/layout/popup.php
+++ code/app/views/layout/popup.php
@@ -3,14 +3,17 @@
         <button class="popup__close" id="pupup-close">х</button>
         <div>
             <h2>Создать группу</h2>
-            <form id="create-group-form">
+            <form id="search-own-contacts-form">
                 <input type="text" name="search">
                 <input type="submit" value="Найти">
             </form>
             <div class="popup__content__users">
                 <ul class="popup__content__users__list"></ul>
             </div>
-            <button class="popup__content__create">Создать</button>
+            <form action="" id="create-group-form">
+                <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?? '' ?>">
+                <button class="popup__content__create">Создать</button>
+            </form>
         </div>
     </div>
 </div>
