340d1188955adce87272d7facee25f71454669c2
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-08 01:37:11 +0300

    Add createGroup method to api ChatController

--- code/app/api/ChatController.php
+++ code/app/api/ChatController.php
@@ -4,6 +4,7 @@ namespace App\Api;
 
 use App\Core\Controller;
 use App\Models\Message;
+use App\Models\Chat;
 use function App\Tools\validateCsrfToken;
 use function App\Tools\refreshCsrfToken;
 
@@ -46,5 +47,54 @@ class ChatController extends Controller
 
         echo json_encode($response);
         exit;
+    }
+
+    public function createGroup()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $csrfToken = $data['csrf_token'] ?? '';
+
+        if (!validateCsrfToken($csrfToken)) {
+            refreshCsrfToken();
+            $response['status'] = 'failed';
+            $response['message'] = 'Проверка токена CSRF не удалась.';
+            $response['csrf_token'] = $_SESSION['csrf_token'];
+            http_response_code(403);
+            echo json_encode($response);
+            exit;
+        }
+
+        $users = $data['users'];
+        $chatId = Chat::create(['user_id' => $_SESSION['user']['id'], 'is_admin' => 1, 'is_group' => 1]);
+
+        if (!$chatId) {
+            $response['status'] = 'failed';
+            $response['message'] = 'Не удалось создать чат';
+            echo json_encode($response);
+            exit;
+        }
+
+        $isAdded = false;
+
+        foreach ($users as $user) {
+            $addUser = Chat::addMember(['chat_id' => $chatId, 'user_id' => $user]);
+            $isAdded = $isAdded || $addUser;
+        }
+
+        if (!$isAdded) {
+            $response['status'] = 'failed';
+            $response['message'] = 'Не удалось добавить пользователей';
+            echo json_encode($response);
+            exit;
+        }
+
+        refreshCsrfToken();
+
+        $response['csrf_token'] = $_SESSION['csrf_token'];
+        $response['status'] = 'success';
+        $response['chat_id'] = $chatId;
+        echo json_encode($response);
+        exit;
+
     }
 }
