657de188a649656324c30425c73ee1be31ee165d
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-08 01:40:37 +0300

    Edit ChatController: edit index method, add group method, add private getChatsAndGroups method

--- code/app/controllers/ChatController.php
+++ code/app/controllers/ChatController.php
@@ -11,7 +11,103 @@ class ChatController extends Controller
     public function index()
     {
         parent::auth();
-        $usersChats = User::getChats(false);
+
+        [$chats, $groups] = $this->getChatsAndGroups();
+
+        if (!isset($_GET['user'])) {
+            $this->view->render([
+                'content_view' => '/chat/chat_view.php',
+                'title' => 'Чат',
+                'data' => ['chats' => $chats, 'groups' => $groups]
+            ]);
+            exit;
+        }
+
+        $contactId = $_GET['user'];
+        $contact = User::getUser($contactId);
+        $contactName = null;
+
+        if ($contact) {
+            $contactName =  $contact['username'] != '' ? $contact['username'] : $contact['email'];
+        }
+
+        $chatId = null;
+        $messages = null;
+        $errors = null;
+
+        if ($contact){
+            $chatId = Chat::getDialog(['user_id' => $_SESSION['user']['id'], 'contact_id' => $contactId]);
+
+            if (!$chatId) {
+                $chatId = Chat::create(['user_id' => $_SESSION['user']['id'], 'is_admin' => 0, 'is_group' => 0]);
+                if (!$chatId) {
+                    $errors['user'] = 'Не удалось создать чат';
+                } else {
+                    $addUser = Chat::addMember(['chat_id' => $chatId, 'user_id' => $contactId]);
+                    if (!$addUser) {
+                        Chat::destroy($chatId);
+                        $errors['user'] = 'Не удалось добавить в чат';
+                    }
+                }
+            }
+
+            $messages = Chat::getMessages(['chat_id' => $chatId]);
+        } else {
+            $errors['user'] = 'Пользователь не найден';
+        }
+
+        $this->view->render([
+            'content_view' => '/chat/chat_view.php',
+            'title' => 'Чат',
+            'data' => [
+                'chat_id' => $chatId,
+                'chats' => $chats,
+                'groups' => $groups,
+                'contact' => [
+                    'id' => $contactId,
+                    'name' => $contactName
+                ],
+                'messages' => $messages,
+                'errors' => $errors
+            ]
+        ]);
+    }
+
+    public function group()
+    {
+        parent::auth();
+
+        $errors = null;
+
+        [$chats, $groups] = $this->getChatsAndGroups();
+
+        if (!isset($_GET['id'])) {
+            $this->view->render([
+                'content_view' => '/chat/chat_view.php',
+                'title' => 'Чат',
+                'data' => ['chats' => $chats, 'groups' => $groups]
+            ]);
+            exit;
+        }
+        $groupId = $_GET['id'];
+        $messages = Chat::getMessages(['chat_id' => $groupId]);
+
+        $this->view->render([
+            'content_view' => '/chat/chat_view.php',
+            'title' => 'Чат',
+            'data' => [
+                'chat_id' => $groupId,
+                'chats' => $chats,
+                'groups' => $groups,
+                'messages' => $messages,
+                'errors' => $errors
+            ]
+        ]);
+    }
+
+    private function getChatsAndGroups()
+    {
+        $usersChats = User::getChats(['user_id' => $_SESSION['user']['id'], 'is_group' => false]);
         $chats = [];
         $chatsWithNames = [];
 
@@ -28,60 +124,16 @@ class ChatController extends Controller
                         $chatsWithNames[] = [
                             'id' => $chat['chat_id'],
                             'user_id' => $memberInfo['id'],
-                            'title' => $memberInfo['username'] != '' ? $memberInfo['username'] : $memberInfo['email']
+                            'title' => $memberInfo['username'] != '' ? $memberInfo['username'] : $memberInfo['email'],
+                            'avatar' => $memberInfo['avatar'],
                         ];
                     }
                 }
             }
         }
 
-        if (!isset($_GET['user'])) {
-            $this->view->render([
-                'content_view' => '/chat/chat_view.php',
-                'title' => 'Чат',
-                'data' => ['chats' => $chatsWithNames]
-            ]);
-            exit;
-        }
+        $groups = User::getChats(['user_id' => $_SESSION['user']['id'], 'is_group' => true]);
 
-        $contactId = $_GET['user'];
-
-        $contact = User::getUser($contactId);
-        $contactName = null;
-
-        if ($contact) {
-            $contactName =  $contact['username'] != '' ? $contact['username'] : $contact['email'];
-        }
-
-        $chatId = null;
-        $messages = null;
-        $errors = null;
-
-        if ($contact){
-            $chatId = Chat::getDialog(['user_id' => $_SESSION['user']['id'], 'contact_id' => $contactId]);
-
-            if (!$chatId) {
-                $chatId = Chat::create(['user_id' => $_SESSION['user']['id'], 'contact_id' => $contactId, 'is_group' => 0]);
-            }
-
-            $messages = Chat::getMessages(['chat_id' => $chatId]);
-        } else {
-            $errors['user'] = 'Пользователь не найден';
-        }
-
-        $this->view->render([
-            'content_view' => '/chat/chat_view.php',
-            'title' => 'Чат',
-            'data' => [
-                'chat_id' => $chatId,
-                'chats' => $chatsWithNames,
-                'contact' => [
-                    'id' => $contactId,
-                    'name' => $contactName
-                ],
-                'messages' => $messages,
-                'errors' => $errors
-            ]
-        ]);
+        return [$chatsWithNames, $groups];
     }
 }
