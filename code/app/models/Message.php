08fd9082daac72c082c6921b59bcaaba9162f39d
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-08 20:03:08 +0300

    Add isOwn, update, destroy, forward methods to Message model

--- code/app/models/Message.php
+++ code/app/models/Message.php
@@ -69,4 +69,109 @@ class Message extends Model {
         }
         return false;
     }
+
+    /**
+     * Check if message belongs to user
+     * @param array $data - associative array. keys - [id, user_id]
+     * @return bool
+     */
+    public static function isOwn($data)
+    {
+        try {
+            $db = DB::connect();
+            $query = 'SELECT * FROM messages WHERE id = :id AND user_id = :user_id';
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':id', $data['id']);
+            $stmt->bindParam(':user_id', $data['user_id']);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            if ($result) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return false;
+    }
+
+    /**
+     * Update message
+     * @param array $data - associative array. keys - [id, content]
+     * @return array|bool - associative array. keys - [updated_at]
+     */
+    public static function update($data)
+    {
+        try {
+            $db = DB::connect();
+            $db->beginTransaction();
+            $query = 'UPDATE messages SET content = :content WHERE id = :id';
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':content', $data['content']);
+            $stmt->bindParam(':id', $data['id']);
+            $stmt->execute();
+            if ($stmt) {
+                $query = 'SELECT updated_at FROM messages WHERE id = :id';
+                $stmt = $db->prepare($query);
+                $stmt->bindParam(':id', $data['id']);
+                $stmt->execute();
+                $result = $stmt->fetch();
+                if ($result) {
+                    $db->commit();
+                    return $result;
+                } else {
+                    $db->rollBack();
+                }
+            } else {
+                $db->rollBack();
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return false;
+    }
+
+    /**
+     * Delete message
+     * @param int $id
+     * @return bool
+     */
+    public static function destroy($id)
+    {
+        try {
+            $db = DB::connect();
+            $query = 'DELETE FROM messages WHERE id = :id';
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            if ($stmt) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return false;
+    }
+
+    /**
+     * Forward message to chat
+     * @param array $data - associative array. keys - [chat_id, message_id]
+     * @return bool
+     */
+    public static function forward($data)
+    {
+        try {
+            $db = DB::connect();
+            $query = 'INSERT INTO chat_messages (chat_id, message_id) VALUES (:chat_id, :message_id)';
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':chat_id', $data['chat_id']);
+            $stmt->bindParam(':message_id', $data['message_id']);
+            $stmt->execute();
+            if ($stmt) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return false;
+    }
 }
