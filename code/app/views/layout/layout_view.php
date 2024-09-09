1ddfe7cd650d98a17d11c377a430f79210e4c217
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-09-09 14:17:02 +0300

    Add csrf token to meta tag. Add func to update csrf token

--- code/app/views/layout/layout_view.php
+++ code/app/views/layout/layout_view.php
@@ -5,6 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/app/assets/css/style.css">
+    <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?? '' ?>">
+    <script>
+        const csrfToken = document.querySelector('meta[name="csrf-token"]');
+        function updateCsrfToken(newToken) {
+            document.querySelector('meta[name="csrf-token"]').setAttribute('content', newToken);
+
+            const csrfInputs = document.querySelectorAll('input[name="csrf_token"]');
+            csrfInputs.forEach(input => {
+                input.value = newToken;
+            });
+        }
+    </script>
     <title><?php echo $title; ?></title>
 </head>
 
